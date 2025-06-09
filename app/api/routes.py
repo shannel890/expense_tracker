4c165b0cd050f35faaaa9075f7d29038d6b14e8a
shannel890 <shannelkirui739@gmail.com>
2025-06-09 16:28:49 +0300

    modified app/api/routes.py

--- app/api/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-api_bp = Blueprint('api_bp',__name__,url_prefix='/api')
-
-@api_bp.route('/users', methods=['GET'])
-def user_list():
-    return [{"name": "Mark","email": "mark@gmail.com"}]
-
