e5fae809f642b36d58a846289b602d5c8ceed51b
shannel890 <shannelkirui739@gmail.com>
2025-06-09 22:32:03 +0300

    deleted app/models/user.py

--- app/models/user.py
+++ app/models/user.py
@@ -11,7 +11,7 @@ def kenya_time():
     return datetime.now(KENYA_TIMEZONE)
 
 class User(UserMixin,db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
